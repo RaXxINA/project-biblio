aaa30ec72ae4b2beb6cfe8b169b15b750aa21509
RaXxINA <161144359+RaXxINA@users.noreply.github.com>
2024-05-15 23:26:36 +0100

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <windows.h>
 // User structure
 typedef struct {
 	int id;
@@ -33,9 +31,9 @@ void utilisateurMode();
 void ownerMode();
 void displayUserMenu();
 void displayOwnerMenu();
-void modifyUserInfo(char[]);
+void booksInfo(char[]);
 void searchBooks();
-void removeUser(int);
+void removeUser(char[]);
 void addUser(char[]);
 void addBook();
 void removeBook();
@@ -82,52 +80,24 @@ printf("                                 \xB3\xDB\xDB\xDB\xDB\xDB\xDB\xDB\xDB Bo
 // Owner Mode
 void ownerMode() {
     displayOwnerMenu();
-    int c, i,j=0;
+    int c;
     scanf("%d", &c);
     char k[50];
     if(c==1){
        loadUsers();
         }
     if(c==2){
-    	int numusers=0;
-	FILE *userFile = fopen("users.csv", "w");
-    if (userFile== NULL) {
-        printf("Error opening library file for writing.\n");
-        return;
-    }
-    char n[50];
-    while (fscanf(userFile, "%s",n) == 1) {
-	numusers++;
-        char * strToken = strtok ( n, ";" );
-    while ( strToken != NULL ) { 
-        strToken = strtok ( NULL, ";");
-    }
-	
-    }
-numusers/=4;
+    char k[50];
        printf("donner le nom d'utilisateur svp :");
-        scanf("%s", k);i=0;
-            	while (fscanf(userFile, "%s",n) == 1) {
-        char * strToken = strtok ( n, ";" );
-        i++;
-    while ( strToken != NULL ) { 
-    if (strcmp(strToken, k)== 0) {
-                    removeUser(i);j++;
-                }
-        strToken = strtok ( NULL, ";");
-    }}if(j==0){
-                	printf("%s n'exist pas",k);
-                	ownerMode();
-				}
-	
-    
-                
+        scanf("%s", k);
+             removeUser(k);
             }
         
     if(c==3){printf("donner username:");
 	char f[50];
 	scanf("%s",f)	;
 	addUser(f);
+	ownerMode();
         }
     if(c==4){
         addBook();}
@@ -146,51 +116,58 @@ numusers/=4;
 // Utilisateur Mode
 void utilisateurMode() {
         char username[50];
+        fflush(stdin);
         printf("Enter username: ");
         scanf("%s", username);
 
         // Check if user exists
-        char userFound[4];
-		strcpy(userFound,"No");
-        int userIndex,k,i;
-        int numusers=0;
-	FILE *userFile = fopen("users.csv", "w");
+        char userFound[5];
+        int i,numusers=0;
+	FILE *userFile = fopen("users.csv", "r");
     if (userFile== NULL) {
         printf("Error opening library file for writing.\n");
         return;
     }
     char n[50];
     while (fscanf(userFile, "%s",n) == 1) {
-	numusers++;
         char * strToken = strtok ( n, ";" );
-    while ( strToken != NULL ) { 
-        strToken = strtok ( NULL, ";");
+    while (strToken != NULL ) { 
+   if (strcmp(strToken, "Yes") == 0||strcmp(strToken, "No") == 0){
+        numusers++;
     }
-	
+    if (strcmp(strToken,username) == 0){
+        strcpy(userFound,"Yes");
     }
-numusers/=4;
-fclose(userFile);
-        for (i = 0;i <numusers ;i++) {
-            if (strcmp(user[i].username, username) == 0) {
-                strcpy(userFound,"Yes");
-                userIndex=i;
-                break;
-            }
-        }
+	strToken = strtok ( NULL, ";" );
+    }}
+    
 if(userFound == "Yes"){
             char password[50];
             int attempts = 5;
             while (attempts > 0) {
                 printf("Enter password: ");
                 scanf("%s", password);
-                if (strcmp(user[userIndex].password, password) == 0) {
-                    // Display user menu
-                    displayUserMenu();
+                char l[50];
+                while (fscanf(userFile, "%s",l) == 1) {
+        char * strToken = strtok ( l, ";" );
+    while ( strToken != NULL ) { 
+   if (strcmp(strToken, password) == 0){
+        displayUserMenu();
+}else{
+	if(attempts==0){
+		return ;
+	}
+                    attempts--;
+                    printf("Incorrect password %d attempts remaining.\n", attempts);
+                
+}
+	strToken = strtok ( NULL, ";" );
+    }}fclose(userFile);
 	int c;
     scanf("%d", &c);
     if(c==1){
     	int numfeedbacks=0;
-    FILE *feedbackFile = fopen("feedbacks.csv", "w");
+    FILE *feedbackFile = fopen("feedbacks.csv", "a");
     if (feedbackFile == NULL) {
         printf("Error opening feedback file for writing.\n");
         return;
@@ -198,8 +175,11 @@ if(userFound == "Yes"){
 char name[50];
     while (fscanf(feedbackFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
-		numfeedbacks++;
+		
     while ( strToken != NULL ) {
+    	if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	numfeedbacks++;
+	}
         strToken = strtok ( NULL, ";");
     } }
        (numfeedbacks)++;
@@ -208,11 +188,11 @@ strcpy(feedback[numfeedbacks-1].username,username);
     char text[200];
     scanf("%s", feedback[numfeedbacks-1].text);
     for(i=0;i<numfeedbacks;i++){
-    fprintf(feedbackFile, "%d,%s,%s\n",i+1,feedback[i].username,feedback[i].text);}	
+    fprintf(feedbackFile, "%d ; %s ; %s\n",i+1,feedback[i].username,feedback[i].text);}	
     fclose(feedbackFile);
-	  } 
+	 } 
     if(c==2){
-	modifyUserInfo(username);
+	booksInfo(username);
         }
     if(c==3){searchBooks(); 
         }
@@ -220,16 +200,11 @@ strcpy(feedback[numfeedbacks-1].username,username);
 printf("Merci pour utiliser notre application");
         freeall();return;}
         		
-                } else {
-                    attempts--;
-                    printf("Incorrect password. %d attempts remaining.\n", attempts);
-                } }
+                 }
 				if(attempts==0){
-            	printf("You are now Blacklisted");
-            	strcpy(user[userIndex].blacklisted,"Yes");
-            	for(i=0;i<numusers;i++){
-    fprintf(userFile, "%d, %s, %s, %s, %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
-	}fclose(userFile);}
+            	printf("Try again in another time");
+            	freeall();return;}
+            	 
 			
 				}else { fclose(userFile);
             printf("User not found. Creating a new account.\n");
@@ -257,10 +232,10 @@ strcpy(feedback[(numfeedbacks)-1].username,username);
     char text[200];
     scanf("%s", feedback[(numfeedbacks)-1].text);
     for(i=0;i<numfeedbacks;i++){
-    fprintf(feedbackFile, "%d,%s,%s\n",i+1,feedback[i].username,feedback[i].text);}	
-	fclose(feedbackFile);}
+    fprintf(feedbackFile, "%d ; %s ; %s\n",i+1,feedback[i].username,feedback[i].text);}	
+	fclose(feedbackFile);utilisateurMode();}
     if(c==2){
-	modifyUserInfo(username);
+	booksInfo(username);
         }
     if(c==3){searchBooks(); 
         }
@@ -277,7 +252,7 @@ strcpy(feedback[(numfeedbacks)-1].username,username);
 void displayUserMenu() {
     printf("\nUtilisateur Menu:\n");
     printf("1. Send Feedback\n");
-    printf("2. Modify User Info\n");
+    printf("2. Afficher books Infos\n");
     printf("3. Search Books by title or categorie\n");
     printf("4. Quit\n");
     printf("Enter choice: ");
@@ -298,116 +273,75 @@ void displayOwnerMenu() {
 }
 
 // Function to modify user info
-void modifyUserInfo(char username[]) {
+void booksInfo(char username[]) {
 	int numusers=0;
-	FILE *userFile = fopen("users.csv", "w");
+	FILE *userFile = fopen("users.csv", "a");
     if (userFile== NULL) {
         printf("Error opening library file for writing.\n");
         return;
     }
-    char n[50];
+    char n[50];int userIndex;
     while (fscanf(userFile, "%s",n) == 1) {
-	numusers++;
         char * strToken = strtok ( n, ";" );
+        userIndex = 0;
     while ( strToken != NULL ) { 
+     if (strcmp(strToken, username) == 0) {
+          userIndex = 1;
+        }
+    if(strcmp(strToken,"Ultra")==0  && userIndex == 1){
+    	loadBooks();
+	}
         strToken = strtok ( NULL, ";");
     }
 	
     }
-numusers/=4;
+
+fclose(userFile);
     
-    // Find the user index
- int i,userIndex = -1;
-    for (i = 0; i < numusers; i++) {
-        if (strcmp(user[i].username, username) == 0) {
-            userIndex = i;
-            break;
-        }
-    }
-
-    if (userIndex == -1) {
-        printf("User not found.\n");
-        return;
-    }else{
-
-    printf("Enter new password: ");
-    scanf("%s", user[userIndex].password);
-    fflush(stdin);
-    printf("Enter type of membership : ");
-    scanf("%s", user[userIndex].membership);
-    printf("User info updated successfully.\n");
-    // Save users to file
-    for(i=0;i<numusers;i++){
-    fprintf(userFile, "%d, %s, %s, %s, %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
-	}fclose(userFile);
-    ownerMode();
-}}
+ }
 
 // Function to search books
 void searchBooks(){
-    // Implementation for searching books by title, category, or writer and sorting alphabetically
-    // This can be done by reading the library array and sorting it accordingly
-    // However, for simplicity, let's assume the library array is already sorted alphabetically
-    // We can then implement searching based on title, category, or author using linear search
-    int numbooks=0;
-	FILE *bookFile = fopen("library.csv", "w");
+	FILE *bookFile = fopen("library.csv", "r");
     if (bookFile== NULL) {
-        printf("Error opening library file for writing.\n");
+        printf("Error opening library file for Searching.\n");
         return;
     }
-    char n[50];
-    while (fscanf(bookFile, "%s",n) == 1) {
-	numbooks++;
-        char * strToken = strtok ( n, ";" );
-    while ( strToken != NULL ) { 
-        strToken = strtok ( NULL, ";");
-    }
-	
-    }
-numbooks/=4;
-    
     int g;
 printf("if u want to search by title enter 1 if by category enter 2:");
 scanf("%d",&g);
 char searchTerm[100];
     printf("Enter search term: ");
-    scanf("%s", searchTerm);
+    scanf("%s",searchTerm);
 if(g==1){
 	printf("Search Results:\n");
-    char name[50];int j;
+    char name[50];int j=0;
     while (fscanf(bookFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
-        
     while ( strToken != NULL ) {
-	if(strToken==searchTerm){
-	j=1;
-	}
-        printf ( "%s     ", strToken );
-		
+	if(strcmp(strToken,searchTerm)==0){
+	j=1;}	
         strToken = strtok ( NULL, ";");
     }}
-    printf("\n");
     if(j==1){
 	printf("ce titre %s est disponible dans le biblio\n",searchTerm);
 	}else{
     	printf("ce titre %s n'est pas dans le biblio",searchTerm);
 	}
-	
 }else{
 printf("Search Results:\n");
-    char name[50];int j;
+    char name[50];int j=0;
     while (fscanf(bookFile, "%s",name) == 1) {
-        char * strToken = strtok ( name, ";" );
+        char * strToken = strtok ( name, "; " );
         
     while ( strToken != NULL ) {
-	if(strToken==searchTerm){
-	int	j=1;
+	if(strcmp(strToken,searchTerm)==0){
+	j=1;
 	}
-        printf ( "%s     ", strToken );
-		
-        strToken = strtok ( NULL, ";");
-    }}
-    printf("\n");
+		printf("%s",name);
+        strToken = strtok ( NULL, " ;");
+    }
+	printf("\n");}
     if(j==1){
 	printf("cet auteur %s est disponible dans le biblio\n",searchTerm);
 	}else{
@@ -415,38 +349,55 @@ printf("Search Results:\n");
 	}
     fclose(bookFile);
 }
-ownerMode();
+
 }
 // Function to remove user
-void removeUser(int index) {
-	int numusers=0;
-	FILE *userFile = fopen("users.csv", "w");
+void removeUser(char k[]) {
+	int i=0,j=0,numusers=0;
+	FILE *userFile = fopen("users.csv", "a");
     if (userFile== NULL) {
         printf("Error opening library file for writing.\n");
         return;
     }
     char n[50];
     while (fscanf(userFile, "%s",n) == 1) {
-	numusers++;
+	
         char * strToken = strtok ( n, ";" );
-    while ( strToken != NULL ) { 
-        strToken = strtok ( NULL, ";");
+    while ( strToken != NULL ) {
+    if(j==1){
+    	strcpy(user[i].username,strToken);
+	}
+	if(j==2){
+    	strcpy(user[i].password,strToken);
+	}
+	if(j==3){
+    	strcpy(user[i].membership,strToken);
+	}
+   if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+	   	numusers++;
+		   strcpy(user[i].blacklisted,strToken); 
+	}
+	if(j==4){
+		j=-1;i++;
+	}
+		strToken = strtok ( NULL, ";");
+		j++;
     }
 	
     }
-numusers/=4;
-    char username[50];int i;
-    strcpy(username, user[index].username); // Copy username for printing
-    // Shift remaining users to fill the gap
-    for(i =index;i <numusers-1;i++){
-        user[i] = user[i+1];
+    for(i =0;i <numusers;i++){
+    	if(user[i].username==k){
+    		for(int j=i;j<numusers-1;j++){
+    	user[j] = user[j+1];	}	
+		}
     }
 numusers--;
-    printf("User '%s' removed successfully.\n", username);
-
+    printf("User '%s' removed successfully.\n",k);
+fclose(userFile);
+userFile = fopen("users.csv", "w");
     // Save users to file
     for(i=0;i<numusers;i++){
-    fprintf(userFile, "%d, %s, %s, %s, %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
+    fprintf(userFile, "%d ; %s ; %s ; %s ; %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
 	}fclose(userFile);
     ownerMode();
 }
@@ -454,28 +405,23 @@ numusers--;
 // Function to add user
 void addUser(char user1[]) {
 	int numusers=0;
-	FILE *userFile = fopen("users.csv", "w");
+	FILE *userFile = fopen("users.csv", "a");
     if (userFile== NULL) {
         printf("Error opening library file for writing.\n");
         return;
     }
     char n[50];
     while (fscanf(userFile, "%s",n) == 1) {
-	numusers++;
+	
         char * strToken = strtok ( n, ";" );
     while ( strToken != NULL ) { 
+    if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	numusers++;
+	}
         strToken = strtok ( NULL, ";");
     }
-	
     }
-numusers/=4;
-    
-User newUser;int i,k=0;
-           for (i=0;i<numusers;i++){
-        if (strcmp(user[i].username,user1)==0){
-            printf("Username already exists.\n");
-            k++;}}
-        if (k==0){
+User newUser;int i;
         	strcpy(newUser.username,user1);
     printf("Enter password: ");
     scanf("%s", newUser.password);
@@ -484,12 +430,8 @@ User newUser;int i,k=0;
     strcpy(newUser.blacklisted,"No");
 (numusers)++;
     user[(numusers)-1] =newUser;
-    printf("User added successfully at index %d.\n",(numusers)-1);
-
-		}
-    
         for(i=0;i<numusers;i++){
-    fprintf(userFile, "%d, %s, %s, %s, %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
+    fprintf(userFile, "%d ; %s ; %s ; %s ; %s\n",i+1,user[i].username,user[i].password,user[i].membership,user[i].blacklisted);
 	}fclose(userFile);
     // Save users to file
 	
@@ -498,7 +440,7 @@ User newUser;int i,k=0;
 // Function to add book to library
 void addBook() {
 		int i,numbooks=0;
-    FILE *bookFile = fopen("library.csv", "r");
+    FILE *bookFile = fopen("library.csv", "a");
     if (bookFile == NULL) {
         printf("Error opening user file.\n");
         return;
@@ -507,40 +449,39 @@ void addBook() {
     char name[50];
     while (fscanf(bookFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
-        numbooks++;
     while ( strToken != NULL ) {
-        
+        if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	numbooks++;
+	}
 		
         strToken = strtok ( NULL, ";");
     }
-    
-    
     } 
     
     Book newBook;
-    numbooks/=4;
     (numbooks)++;
     printf("Enter title: ");
     scanf("%s", newBook.title);
     printf("Enter author: ");
     scanf("%s", newBook.author);
-	fflush(stdin);
+    fflush(stdin);
     printf("Enter Description: ");
     gets(newBook.description);
     fflush(stdin);
     printf("Enter prix: ");
-    scanf("%f", newBook.prix);
+    scanf("%f",&newBook.prix);
+    fflush(stdin);
     printf("Enter category: ");
     scanf("%s", newBook.category);
+    fflush(stdin);
     printf("Enter disponible ou no: ");
     scanf("%s", newBook.disponible);
     book[numbooks-1]=newBook;
     printf("Book added successfully.\n");
     // Save books to file
     for(i=0;i<numbooks;i++){
-    fprintf(bookFile, "%d, %s, %s\n",i+1,book[i].author,book[i].title,book[i].category);
+    fprintf(bookFile, "%d ; %s ; %s ; %s ; %.2f ; %s ; %s\n",i+1,book[i].title,book[i].author,book[i].description,book[i].prix,book[i].category,book[i].disponible);
 	}
-
     fclose(bookFile);
     ownerMode();
 }
@@ -548,7 +489,7 @@ void addBook() {
 // Function to remove book from library
 void removeBook() {
 		int numbooks=0;
-    FILE *bookFile = fopen("library.csv", "r");
+    FILE *bookFile = fopen("library.csv", "a");
     if (bookFile == NULL) {
         printf("Error opening user file.\n");
         return;
@@ -557,10 +498,11 @@ void removeBook() {
     char name[50];
     while (fscanf(bookFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
-        numbooks++;
-    while ( strToken != NULL ) {
         
-		
+    while ( strToken != NULL ) {
+        if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	numbooks++;
+	}
         strToken = strtok ( NULL, ";");
     }
     
@@ -572,8 +514,7 @@ void removeBook() {
 int i;
     // Find the book index
     int bookIndex = -1;
-    int l;
-    for (i=0;i<numbooks/4;i++) {
+    for (i=0;i<numbooks;i++) {
         if (strcmp(book[i].title, title) == 0) {
             bookIndex = i;
             break;
@@ -585,7 +526,7 @@ int i;
         ownerMode();
     }
     // Shift remaining books to fill the gap
-    for (i=bookIndex;i<(numbooks/4)-1;i++) {
+    for (i=bookIndex;i<(numbooks)-1;i++) {
         book[i]=book[i+1];
     }
     numbooks--;
@@ -593,8 +534,8 @@ int i;
     printf("Book '%s' removed successfully.\n", title);
 
     // Save books to file
-    for(i=0;i<numbooks/4;i++){
-    fprintf(bookFile, "%d, %s, %s\n",i+1,book[i].author,book[i].title,book[i].category);
+    for(i=0;i<numbooks;i++){
+    fprintf(bookFile, "%d ; %s ; %s ; %s ; %.2f ; %s ; %s\n",i+1,book[i].title,book[i].author,book[i].description,book[i].prix,book[i].category,book[i].disponible);
 	}
 
     fclose(bookFile);
@@ -609,20 +550,22 @@ void loadUsers() {
         printf("Error opening user file.\n");
         return;
     }
-    
+    printf("Id:     username:     passsword:     membership:     blacklisted:\n");
     char name[50];
     while (fscanf(userFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
     while ( strToken != NULL ) {
         printf ( "%s     ", strToken );
-		
+		if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	printf("\n");
+	}
         strToken = strtok ( NULL, ";");
     }
-    printf("\n");
+    
     
     } 
     fclose(userFile);
-
+ownerMode();
 }
 
 // Function to load books from file
@@ -632,16 +575,18 @@ void loadBooks() {
         printf("Error opening library file.\n");
         return;
     }
-  
+  printf("Id:     titre:     auteur:     description:     prix:     categorie:     disponibilte:\n");
     char name[50];
     while (fscanf(bookFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
     while ( strToken != NULL ) {
         printf ( "%s     ", strToken );
-		
+		if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	printf("\n");
+	}
         strToken = strtok ( NULL, ";");
     }}
-    printf("\n");
+
     
     fclose(bookFile);
 }
@@ -653,21 +598,23 @@ void loadFeedbacks() {
         printf("Error opening feedback file.\n");
         return;
     } 
-    
+    printf("Id:     username:     feedback:\n");
     char name[50];
     while (fscanf(feedbackFile, "%s",name) == 1) {
         char * strToken = strtok ( name, ";" );
     while ( strToken != NULL ) {
         printf ( "%s     ", strToken );
-		
+		if(strcmp(strToken,"No")==0 ||strcmp(strToken,"Yes")==0){
+    	printf("\n");
+	}
         strToken = strtok ( NULL, ";");
     }
-    printf("\n");
+    
     
     } 
 
     fclose(feedbackFile);
-
+ownerMode();
 }
 void freeall(){
 	free(user);free(book);free(feedback);
